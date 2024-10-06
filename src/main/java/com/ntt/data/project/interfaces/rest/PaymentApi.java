e9a2eb75b6fafcf86ed110b028413df84f902451
CARLOS SANTOS <carlos17vlc@gmail.com>
2024-10-06 10:52:21 +0200

    Update PaymentApi to add internal error.

--- src/main/java/com/ntt/data/project/interfaces/rest/PaymentApi.java
+++ src/main/java/com/ntt/data/project/interfaces/rest/PaymentApi.java
@@ -1,5 +1,6 @@
 package com.ntt.data.project.interfaces.rest;
 
+import com.ntt.data.project.application.dto.PaymentResponse;
 import com.ntt.data.project.application.dto.RegisterPaymentRequest;
 import com.ntt.data.project.domain.model.entities.Payment;
 import io.swagger.v3.oas.annotations.Operation;
@@ -8,8 +9,8 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +24,8 @@ public interface PaymentApi {
                     content = {@Content(mediaType = "application/json",
                             schema = @Schema(implementation = Payment.class))}),
             @ApiResponse(responseCode = "400", description = "Invalid payment request",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
                     content = @Content)
     })
     @PostMapping("/register")
@@ -33,7 +36,9 @@ public interface PaymentApi {
             @ApiResponse(responseCode = "200", description = "List of payments",
                     content = {@Content(mediaType = "application/json",
                             array = @ArraySchema(schema = @Schema(implementation = Payment.class)))}),
-            @ApiResponse(responseCode = "404", description = "Payments not found for the given user ID", content = @Content)
+            @ApiResponse(responseCode = "404", description = "Payments not found for the given user ID", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)
     })
     @GetMapping("/{userId}")
     ResponseEntity<List<Payment>> getPaymentsByUserId(@PathVariable String userId);
